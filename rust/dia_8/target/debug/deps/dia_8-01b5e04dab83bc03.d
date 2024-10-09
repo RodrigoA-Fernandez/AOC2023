ff2d5f9a9bd6ef3d7a7378843f345782f32714e9
Rodrigo <rodrigoalejandro.fernandez@estudiantes.uva.es>
2024-10-09 13:37:43 +0000

    nix-shell hechas

---
+++ rust/dia_8/target/debug/deps/dia_8-01b5e04dab83bc03.d
@@ -0,0 +1,9 @@
+/home/rodrigo/Proyectos/AOC2023/rust/dia_8/target/debug/deps/libdia_8-01b5e04dab83bc03.rmeta: src/main.rs Cargo.toml
+
+/home/rodrigo/Proyectos/AOC2023/rust/dia_8/target/debug/deps/dia_8-01b5e04dab83bc03.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
