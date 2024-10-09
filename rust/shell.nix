0c0e3cc5d54a04a94cc2ea47ec70f8d63f4b7733
Rodrigo <rodrigoalejandro.fernandez@estudiantes.uva.es>
2024-10-09 13:41:01 +0000

    ahora si, shell bien

--- rust/shell.nix
+++ rust/shell.nix
@@ -1,13 +1,28 @@
-let
-  pkgs = import <nixpkgs> {};
-  fenix = pkgs.callPackage "${
-      fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz"
-    }/packages.nix" { };
-in
-  pkgs.mkShell {
-    buildInputs = [
-      fenix.default.rustc
-      pkgs.rustanalyzer
-    ];
-  }
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.11.tar.gz") {}}:
 
+let
+  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/main.tar.gz") {};
+  aoc = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "cargo-aoc";
+    version = "0.3.5";
+
+    buildInputs = with pkgs; [ openssl ];
+
+    nativeBuildInputs = with pkgs; [ pkg-config ];
+
+    src = pkgs.fetchFromGitHub {
+      owner = "gobanos";
+      repo = pname;
+      rev = version;
+      hash = "sha256-tHuT/dsiyliXdl34bFraYp3T3FUgxFnhEUQfc8O197I=";
+    };
+
+    cargoHash = "sha256-lUQwwGJLHLI9bfJiLUUE8j1svBAgbvr+8hKB/bRzwNA=";
+  };
+in
+pkgs.mkShell {
+  packages = [
+    fenix.stable.toolchain
+    aoc
+  ];
+}
